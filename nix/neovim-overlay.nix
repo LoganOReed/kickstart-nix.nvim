2df8ead866d7107bbd605ae4be586245487d28b0
LoganOReed <me@loganreed.org>
2024-05-08 01:02:51 -0400

    Added hex color overlay

--- nix/neovim-overlay.nix
+++ nix/neovim-overlay.nix
@@ -73,6 +73,7 @@ with final.pkgs.lib; let
     eyeliner-nvim # Highlights unique characters for f/F and t/T motions | https://github.com/jinh0/eyeliner.nvim
     nvim-treesitter-textobjects # https://github.com/nvim-treesitter/nvim-treesitter-textobjects/
     nvim-ts-context-commentstring # https://github.com/joosepalviste/nvim-ts-context-commentstring/
+    nvim-highlight-colors # https://github.com/brenoprata10/nvim-highlight-colors/
     # ^ navigation/editing enhancement plugins
     # Useful utilities
     nvim-unception # Prevent nested neovim sessions | nvim-unception
