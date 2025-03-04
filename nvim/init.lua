ca59db7d9527fcc827eb46b06cc80d1dd1fc8c78
LoganOReed <me@loganreed.org>
2025-03-03 21:27:06 -0500

    Fixed Ultisnips path

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,7 +13,8 @@ g.mapleader = " "
 g.maplocalleader = " "
 -- lua snippet format
 -- g.lua_snippets_path = "/home/occam/.snippets/luasnip"
-g.UltiSnipsSnippetDirectories = {"/home/occam/dotfiles/modules/home-manager/nvim/UltiSnips"
+g.UltiSnipsSnippetDirectories = {"/home/occam/dotfiles/modules/home-manager/nvim/UltiSnips",
+                                 "/home/occam/dotfiles/modules/home/nvim/UltiSnips"
                                   -- Used in previous (bad) way of doing snippet files
                                   -- "/home/occam/.snippets/UltiSnips"
                                   }
